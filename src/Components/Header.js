1eb333a08945a227161085089c57ebea081249a4
Bhaskar Nandy <bhaskarndy@gmail.com>
2024-02-12 22:39:22 +0530

    Commit 1

---
+++ src/Components/Header.js
@@ -0,0 +1,14 @@
+import { React } from 'react';
+import '../CSS for Components/Header.css'
+
+function Header() {
+
+    return (
+        <>
+
+        </>
+    );
+}
+
+export default Header;
+
---
+++ src/Components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './Header'
+import Body from './Body'
+
+function Home() {
+    return (
+        <>
+            <Body />
+        </>
+    );
+}
+
+export default Home;
+
